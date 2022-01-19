a846332d2f7c955e040f807f8d5c504ffa716b57
Ryan Webster <ryan.h.webster@gmail.com>
2022-01-19 15:53:32 -0800

    start of EODs

---
+++ Wk1/day2.js
@@ -0,0 +1,41 @@
+// isPrime (for loop && while loop)
+
+/*
+Define a function isPrime(number) that returns true if number is prime. Otherwise,
+return false. A number is prime if it is only divisible by 1 and itself.
+*/
+
+/*
+Understand the Problem
+Sample Input:
+Sample Output:
+Rules:
+
+*/
+
+/*
+Make a plan
+Steps:
+
+*/
+
+
+//choosePrimes
+/*
+Define a function isPrime(number) that returns true if number is prime. Otherwise, 
+return false. A number is prime if it is only divisible by 1 and itself.
+*/
+
+/*
+Understand the Problem
+Sample Input:
+Sample Output:
+Rules:
+
+*/
+
+/*
+Make a plan
+Steps:
+
+*/
